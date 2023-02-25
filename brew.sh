62acb1e2c6d23a119fe0fb5df893b15fcaccd820
m0xAy <126299768+m0xAy@users.noreply.github.com>
2023-02-25 16:59:47 +1300

    add brew.sh

---
+++ brew.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+brew update
+
+brew install fd
+brew install neovim
+brew install node
+brew install ripgrep
+brew install wget
+brew install tree-sitter
+
+brew install brave-browser
+brew install google-chrome
+brew install firefox
+brew install microsoft-edge
+brew install waterfox
+brew install kitty
+brew install visual-studio-code
+
+brew tap homebrew/cask-fonts
+
+brew install font-fira-code-nerd-font
+brew install font-sf-mono
